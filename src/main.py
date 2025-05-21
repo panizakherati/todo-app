66ff59b8a7fc12476422172385d8dab9e9798c02
Paniz AKHERATI <panizakherati@gmail.com>
2025-05-21 19:17:37 +0000

    initialize functions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+def load_tasks():
+    pass
+def save_tasks():
+    pass
+def show_tasks():
+    pass
+def add_task():
+    pass
+def complete_task():
+    pass
+
+
+
 def main():
     print("Hello from todo-app!")
 
