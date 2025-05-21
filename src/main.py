ae1ea23ec4d25605d38b8c7b7a17a1bef05949f3
Paniz AKHERATI <87237112+panizakherati@users.noreply.github.com>
2025-05-21 22:18:41 +0200

    Load tasks and save tasks added (#1)
    * initialize functions
    
    * load tasks implemented
    
    * fix: error handling added
    
    * feat:save task function implemented
    
    * feat: indentation added
    
    ---------
    
    Co-authored-by: Paniz AKHERATI <panizakherati@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
+import json
+import os
+TODO_FILE = "tasks.json"
+
+def load_tasks():
+    if os.path.exists(TODO_FILE):
+        with open(TODO_FILE, 'r') as f:
+            return json.load(f)
+    return []
+def save_tasks(data):
+    with open(TODO_FILE, 'w') as f:
+        json.dump(data,f, indent=2)
+def show_tasks():
+    pass
+def add_task():
+    pass
+def complete_task():
+    pass
+
+
+
 def main():
     print("Hello from todo-app!")
-
+    tasks = load_tasks()
 
 if __name__ == "__main__":
     main()
